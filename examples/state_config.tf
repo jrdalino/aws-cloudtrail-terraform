d3588e8e5d292df8e06c68ad5751936abe4b2528
Jose Dalino <3316034+jrdalino@users.noreply.github.com>
2021-03-24 23:51:34 +0800

    Update state_config.tf

--- examples/state_config.tf
+++ examples/state_config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "ap-southeast-1"
     bucket         = "623552185285-ap-southeast-1-terraform-state"
-    key            = "bbsdm-organisation-aws-cloudtrail-terraform.tfstate"
+    key            = "bbsdm-shared-aws-cloudtrail-terraform.tfstate"
     encrypt        = "true"
     dynamodb_table = "terraform-state-lock"
   }
